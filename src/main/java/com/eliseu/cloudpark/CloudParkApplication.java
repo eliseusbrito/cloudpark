4e776d3d3cae54f075aba88bedc8e0eec62708d0
eliseusbrito <eliseusbrito@gmail.com>
2022-08-15 20:45:55 -0300

    created project in start.spring.io

---
+++ src/main/java/com/eliseu/cloudpark/CloudParkApplication.java
@@ -0,0 +1,13 @@
+package com.eliseu.cloudpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParkApplication.class, args);
+	}
+
+}
