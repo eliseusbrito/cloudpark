1765a16b7dfad67450a35e1491a929ab3215638b
eliseusbrito <eliseusbrito@gmail.com>
2022-08-21 08:28:02 -0300

    Adicionado service com random UUID

--- src/main/java/com/eliseu/cloudpark/controller/ParkingController.java
+++ src/main/java/com/eliseu/cloudpark/controller/ParkingController.java
@@ -1,26 +1,26 @@
-package com.eliseu.cloudpark.Controller;
+package com.eliseu.cloudpark.controller;
 
 import com.eliseu.cloudpark.model.Parking;
+import com.eliseu.cloudpark.service.ParkingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
 public class ParkingController {
 
+    private final ParkingService parkingService;
+
+    public ParkingController(ParkingService parkingService) {
+        this.parkingService = parkingService;
+    }
 
     @GetMapping
     public List<Parking> findAll() {
-        var parking = new Parking();
-        parking.setColor("PRETO");
-        parking.setLicense("MSS-1111");
-        parking.setModel("VW GOL");
-        parking.setState("SP");
-        return Arrays.asList(parking);
+        return parkingService.findAll();
     }
 
 
