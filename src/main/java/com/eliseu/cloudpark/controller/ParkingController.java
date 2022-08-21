ed4e782ab6c8e2675077b4d067df393990b4b680
eliseusbrito <eliseusbrito@gmail.com>
2022-08-21 09:23:19 -0300

    Adicionado modelMapper para fazer o dto

--- src/main/java/com/eliseu/cloudpark/controller/ParkingController.java
+++ src/main/java/com/eliseu/cloudpark/controller/ParkingController.java
@@ -1,5 +1,7 @@
 package com.eliseu.cloudpark.controller;
 
+import com.eliseu.cloudpark.controller.dto.ParkingDTO;
+import com.eliseu.cloudpark.controller.mapper.ParkingMapper;
 import com.eliseu.cloudpark.model.Parking;
 import com.eliseu.cloudpark.service.ParkingService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +15,17 @@ import java.util.List;
 public class ParkingController {
 
     private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
 
-    public ParkingController(ParkingService parkingService) {
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
         this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
     }
 
     @GetMapping
-    public List<Parking> findAll() {
-        return parkingService.findAll();
+    public List<ParkingDTO> findAll() {
+        List<Parking> parkingList = parkingService.findAll();
+        return parkingMapper.toParkingDTOList(parkingList);
     }
-
 
 }
